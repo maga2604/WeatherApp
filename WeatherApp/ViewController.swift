4d2c6d7b063b7a37c29ee1ba7870d050dc36994c
Maga <maga.rajic2604@gmail.com>
2021-09-25 18:49:00 -0700

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Maga Rajic on 25.09.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
